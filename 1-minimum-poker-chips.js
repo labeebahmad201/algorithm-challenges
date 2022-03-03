ae2af9e9aa87b5c04af823dfb370b4d58a96498d
Labeeb <44992984+labeebahmad201@users.noreply.github.com>
2022-03-03 01:02:20 -0500

    Update 1-minimum-poker-chips.js

--- 1-minimum-poker-chips.js
+++ 1-minimum-poker-chips.js
@@ -1,5 +1,5 @@
 function getMinimumPokerChips(pokerChipsArr){
-    pokerChipsArr.sort((a, b)=>  a > b ? -1 : 1); //nlogn propbably
+    pokerChipsArr.sort((a, b)=>  a > b ? -1 : 1); //nlogn for longer array
     
     let originalTarget = 126;
     let remainingTarget = originalTarget;
@@ -7,7 +7,7 @@ function getMinimumPokerChips(pokerChipsArr){
     
     let i = 0;
     let sumAccountedSoFar = 0;
-    while(i < pokerChipsArr.length){
+    while(i < pokerChipsArr.length){ // O(n)
         if(pokerChipsArr[i] <= remainingTarget){
             let chipsApplicable = parseInt(remainingTarget / pokerChipsArr[i]);
             if(chipsApplicable > 0){
@@ -21,6 +21,8 @@ function getMinimumPokerChips(pokerChipsArr){
 
     return pokerChipsSelected.length > 0 ? pokerChipsSelected : false;
 }
+
+// nlogn + n => n(log+1);
 
 let pokerChipsArrGlobal = [100, 50, 25, 10, 5, 1];
 console.log('UC 1', getMinimumPokerChips(pokerChipsArrGlobal));
