f6c2a762a00830eef6356b255b6a42d7bcc43208
Labeeb <44992984+labeebahmad201@users.noreply.github.com>
2022-03-06 03:31:01 -0500

    Update 5-non constructible change.js

--- 5-non constructible change.js
+++ 5-non constructible change.js
@@ -1,1 +1,22 @@
 
+function nonConstructibleChange(coins) {
+
+	coins.sort((x, y)=>x > y ? 1: -1);
+
+	let change = 0;
+	for(let i = 0; i < coins.length; i++){
+		let current = coins[i];
+
+		if(change + 1 < current){
+			return change + 1; // current+1 is what you cannot make. 
+		}
+		change += current;		
+	}
+	
+	return change + 1;
+
+}
+
+let coins = [1, 1, 1, 1, 1];
+nonConstructibleChange(coins);
+
