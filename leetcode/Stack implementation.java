1d8773946580d273902aeef81436891e2382fc38
Labeeb <44992984+labeebahmad201@users.noreply.github.com>
2025-01-31 13:55:32 -0500

    feat: added stack implementation

--- leetcode/Stack implementation.java
+++ leetcode/Stack implementation.java
@@ -1,7 +1,9 @@
+// "static void main" must be defined in a public class.
 public class MyStack {
     // is buffer the right name?t
     private int[] buffer;
     private int top;
+    private int size;
    
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -11,6 +13,11 @@ public class MyStack {
         stack.push(3);
         stack.push(4);
         stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        System.out.println("isFull : " + stack.isFull());
+        stack.push(8);        
+        System.out.println("isFull : " + stack.isFull());        
         
         stack.print();
         System.out.println("Popped :" + stack.pop());
@@ -19,11 +26,13 @@ public class MyStack {
         System.out.println("Popped :" + stack.pop());
         System.out.println("Popped :" + stack.pop());
         System.out.println("Popped :" + stack.pop());
+        stack.print();        
     }
     
     MyStack(int size) {
         this.buffer = new int[size];
         this.top = -1;
+        this.size = size;
     }
     
     boolean push(int value){
@@ -53,7 +62,7 @@ public class MyStack {
     }
     
     boolean isFull() {
-        return this.top + 1 == this.size;
+        return this.top + 1 == this.buffer.length;
     }
     
     boolean isEmpty() {
@@ -62,7 +71,7 @@ public class MyStack {
     
     void print(){
         System.out.println("printed stack:");
-        for(int i = this.buffer.length - 1; i >= 0; i--){
+        for(int i = this.top; i >= 0; i--){
             System.out.println(this.buffer[i]);
         }
     }
