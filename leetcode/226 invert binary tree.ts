34ffcf911882b78adcc0847b82542d5c052a46a5
Labeeb <44992984+labeebahmad201@users.noreply.github.com>
2025-02-08 22:33:10 -0500

    feat: invert binary tree

--- leetcode/226 invert binary tree.ts
+++ leetcode/226 invert binary tree.ts
@@ -24,3 +24,28 @@ function invertTree(root: TreeNode | null): TreeNode | null {
 
     return root;
 };
+
+
+/// following is an inplace implementation.
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if(!root) {
+        return null;
+    }
+    return new TreeNode(root.val, invertTree(root.right), invertTree(root.left));
+};
