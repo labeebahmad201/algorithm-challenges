8decf3bb132fd156b03a616c0caa25f8ebd08fb4
Labeeb <44992984+labeebahmad201@users.noreply.github.com>
2025-02-08 19:03:46 -0500

    feat: implement is balanced tree function

---
+++ leetcode/110 - balanced tree.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function getHeight (root: TreeNode | null) : number {
+    if(root === null) {
+        return 0;
+    }
+
+    const leftHeight = getHeight(root.left);
+    const rightHeight = getHeight(root.right);
+    if(leftHeight === -1 || rightHeight === -1) {
+        return -1;
+    }
+
+    if(Math.abs(leftHeight - rightHeight) <= 1) {
+        // then it is balanced
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    return -1;
+
+}
+
+function isBalanced(root: TreeNode | null): boolean {
+    return getHeight(root) !== -1;
+};
