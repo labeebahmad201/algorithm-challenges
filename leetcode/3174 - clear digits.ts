9af1161b9d170840fbf699a26557d2938960fb26
Labeeb <44992984+labeebahmad201@users.noreply.github.com>
2025-02-10 06:05:17 -0500

    feat: clear digits

---
+++ leetcode/3174 - clear digits.ts
@@ -0,0 +1,20 @@
+/***
+ * Intuition: 
+ * We need a data structure that supports removal at the end such as stack. 
+ * That way when we come accross a digit & we need to remove closest string, we can pop it from the stack.
+ * 
+ */
+
+function clearDigits(s: string): string {
+    const stack = [];
+    for(let i =0; i < s.length; i++){
+        let ele = parseInt(s[i]);
+        if(isNaN(ele)) { // which means it is a string
+            stack.push(s[i]);
+        } else {
+            stack.pop();
+        }
+    }
+
+    return stack.join('');
+};
