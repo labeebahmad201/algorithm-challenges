962194a30e89922cc720bcac17e3f1ed2670887a
Labeeb <44992984+labeebahmad201@users.noreply.github.com>
2025-02-12 07:59:12 -0500

    feat: xor condition in tree - can be used as generic pattern too

---
+++ leetcode/xor condition.ts
@@ -0,0 +1,7 @@
+// xor condition in tree. 
+
+if (tree === null && tree2 === null) {
+    // Both are null → do nothing or return
+} else if (tree === null || tree2 === null) {
+    // Exactly one is null → XOR behavior
+}
