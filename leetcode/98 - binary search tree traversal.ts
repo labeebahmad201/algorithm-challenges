fce6ae429f4096768dbcb398b244da3a86c99f65
Labeeb <44992984+labeebahmad201@users.noreply.github.com>
2025-02-14 15:23:07 -0500

    feat: 98 - binary search tree traversal

--- leetcode/98 - binary search tree traversal.ts
+++ leetcode/98 - binary search tree traversal.ts
@@ -39,3 +39,57 @@ function isValidBST(root: TreeNode | null): boolean {
 
     return true;
 };
+
+/// ___________________________
+// Second more efficient solution
+
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+/****
+ * Intuition:
+ * To derive this solution you first need to think in terms of contraints applied on a node. 
+ * a node can be BST if it is greater than the left subtree and less than the right subtree.
+ * also it's left and right subtree should be BST.
+ * 
+ * for left child we pass current as max value 
+ * for right child we pass current as min value
+ * 
+ * We consider null node as valid bst because it doesnt violate the constraints on BST.
+ * 
+ * then we have an && condition that makes sure both left and right subtree are BST.
+ * 
+ * for the first call we use -Infinity and +Infinity as min and max values since there are no contraints on root node it can be anything.
+ * 
+ * but as we move down the we apply contraints. if we only keep going left. the only contraint that is applied is max value. 
+ * if we only keep going right the only contraint that is applied is min value.
+ * 
+ * but if going down left of right when we chang direction that's when another contrains has to be applied. 
+ * that's why we are using both min and max values.
+ *  
+ * 
+ */
+
+function isValidBST(root: TreeNode | null): boolean {
+    function dfs(root: TreeNode | null, minVal, maxVal): boolean {
+     if(root === null) return true;
+ 
+     if(!(minVal < root.val && root.val < maxVal)) return false;
+ 
+     return dfs(root.left, minVal, root.val) && dfs(root.right, root.val, maxVal);
+    }
+ 
+    return dfs(root, -Infinity, +Infinity);
+ };
